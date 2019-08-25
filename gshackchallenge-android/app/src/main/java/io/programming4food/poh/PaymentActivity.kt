b79baceff779acc50c809cb0ea848aea9115e5c1
Marco Ramírez <marco.ramirez@rzerocorp.com>
2019-08-25 02:42:42 -0500

    Joining the payment flow

--- gshackchallenge-android/app/src/main/java/io/programming4food/poh/PaymentActivity.kt
+++ gshackchallenge-android/app/src/main/java/io/programming4food/poh/PaymentActivity.kt
@@ -11,6 +11,11 @@ import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import androidx.core.content.ContextCompat.startActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+
 
 class PaymentActivity : AppCompatActivity() {
 
@@ -30,11 +35,37 @@ class PaymentActivity : AppCompatActivity() {
 
         var bottomSheet = findViewById<LinearLayout>(R.id.bottom_sheet_payment)
         var accessCode = findViewById<Button>(R.id.btn_credit_card)
+        var creditBtn = findViewById<Button>(R.id.btn_credito_azteca)
+        var payBtn = findViewById<Button>(R.id.btn_pay_credit_card)
         var bsb = BottomSheetBehavior.from(bottomSheet)
 
         accessCode.setOnClickListener(object: View.OnClickListener {
             override fun onClick(p0: View?) {
                 bsb.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        })
+
+        creditBtn.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val urlString = "https://www.elektra.com.mx/credito-elektra"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlString))
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                intent.setPackage("com.android.chrome")
+                try {
+                    startActivity(intent)
+                } catch (ex: ActivityNotFoundException) {
+                    // Chrome browser presumably not installed and open Kindle Browser
+                    intent.setPackage("com.amazon.cloud9")
+                    startActivity(intent)
+                }
+
+            }
+        })
+
+        payBtn.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                var i: Intent = Intent(this@PaymentActivity, PaymentResultActivity::class.java)
+                startActivity(i)
             }
         })
 
